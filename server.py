29da9e9d46e57f542330b2e5954482aa2da63077
Zimu Zeng <zimu.zeng@gmail.com>
2018-08-25 22:23:37 +0100

    J

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import sqlite3
 from flask import Flask, send_file
 app = Flask(__name__)
 
+@app.route("/")
+def indexPage():
+    return send_file("index.html")
+
 @app.route("/<path:path>")
 def catchAll(path):
-    if path == "":
-        return send_file("index.html")
     return send_file(path)
 
 if __name__ == "__main__":
