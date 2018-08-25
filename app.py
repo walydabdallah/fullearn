097ee87ec20cdbbdb0ecddd311a34ad1196436e7
Zimu Zeng <zimu.zeng@gmail.com>
2018-08-25 23:33:10 +0100

    Landing page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def indexPage():
-    return send_file("index.html")
+    return send_file("preorder.html")
 
 @app.route("/<path:path>")
 def catchAll(path):
