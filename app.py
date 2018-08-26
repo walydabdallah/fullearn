cdf58f39473d26230f670294296b2ec5b98da896
Zimu Zeng <zimu.zeng@gmail.com>
2018-08-26 13:23:22 +0100

    Landing page complete, both frontend and backend

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 import sqlite3
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 app = Flask(__name__)
 
 @app.route("/")
 def indexPage():
     return send_file("landing.html")
 
-@app.route("/preorder", methods=["POST"])
+@app.route("/notifyme", methods=["POST"])
 def preorder():
-    return send_file("html/ltr/vertical-menu-template/coming-soon.html")
+    db = sqlite3.connect("notify.db")
+    cursor = db.cursor()
+    name = request.form["name"]
+    email = request.form["email"]
+    cursor.execute("select id from people;")
+    id = len(cursor.fetchall()) + 1
+    cursor.execute("insert into people values (" + str(id) + ", '" + name + "', '" + email + "');")
+    db.commit()
+    db.close()
+    return send_file("notifyme.html")
 
 @app.route("/<path:path>")
 def catchAll(path):
+    print(path)
     return send_file(path)
 
 if __name__ == "__main__":
